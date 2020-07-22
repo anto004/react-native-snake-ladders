b84bc64ccb1b81d1c3011d1ffcbda20e05f0de11
anto004 <anto_004@live.com>
2020-07-22 07:00:56 +0530

    Updates

---
+++ precache-manifest.b31e2d2291ea355d1c2d8a7933dbd6b0.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cf75cb260446a2f9513",
+    "url": "/react-native-snake-ladders/static/js/app.d87e3221.chunk.js"
+  },
+  {
+    "revision": "f631b26884aa3f84376c",
+    "url": "/react-native-snake-ladders/static/js/runtime~app.14cee608.js"
+  },
+  {
+    "revision": "7de3115dc068cbee2cca",
+    "url": "/react-native-snake-ladders/static/js/2.dc2a3a61.chunk.js"
+  },
+  {
+    "revision": "004c2bbb035d8d06bb830efc4673c886",
+    "url": "/react-native-snake-ladders/static/media/star.69c71386.png"
+  },
+  {
+    "revision": "2327736b3ea09c41abfb69be1221f430",
+    "url": "/react-native-snake-ladders/static/media/heart.23b5c2c0.png"
+  },
+  {
+    "revision": "51671417ef20e0bbc32f0a2bc6edfa95",
+    "url": "/react-native-snake-ladders/static/media/rocket.d2f3bf41.png"
+  },
+  {
+    "revision": "a7b9dc9de5f8f1fb1afbef917619a5ac",
+    "url": "/react-native-snake-ladders/static/media/bell.cf04af27.png"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/react-native-snake-ladders/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/react-native-snake-ladders/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/react-native-snake-ladders/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/react-native-snake-ladders/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/react-native-snake-ladders/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-native-snake-ladders/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/react-native-snake-ladders/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/react-native-snake-ladders/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/react-native-snake-ladders/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/react-native-snake-ladders/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/react-native-snake-ladders/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/react-native-snake-ladders/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "f3f68bf5b7dce8acae55c25f724625dd",
+    "url": "/react-native-snake-ladders/expo-service-worker.js"
+  },
+  {
+    "revision": "0b30ac158cb553f2f93a9c6a75908a36",
+    "url": "/react-native-snake-ladders/index.html"
+  },
+  {
+    "revision": "24ea47fcfd37bfb0232bc534ba3b1274",
+    "url": "/react-native-snake-ladders/register-service-worker.js"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/react-native-snake-ladders/serve.json"
+  },
+  {
+    "revision": "d9b8742bc2250bfdfd710941e991e371",
+    "url": "/react-native-snake-ladders/static/js/2.dc2a3a61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521f8c844d3af13c3d20ba01b2fc5274",
+    "url": "/react-native-snake-ladders/favicon-16.png"
+  },
+  {
+    "revision": "d50b3a5941cfe755e600a75113771581",
+    "url": "/react-native-snake-ladders/favicon-32.png"
+  },
+  {
+    "revision": "fa6155203a3b9f3590820927f5a5991a",
+    "url": "/react-native-snake-ladders/favicon.ico"
+  },
+  {
+    "revision": "708c592e4ce91382133867b7749afe40",
+    "url": "/react-native-snake-ladders/manifest.json"
+  }
+];
